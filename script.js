c4984a096541ca838c39fb1b13d8d640910a5cd8
rasprogrammer <rasprogrammer@gmail.com>
2025-06-20 17:05:35 +0530

    Typing feature is working now with basic result format with timer

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 const modeElement = document.querySelector('.mode');
 const detailsElement = document.querySelector('.mode-setting');
 
+const textContent = "Filler text is text that shares some characteristics of a real written text, but is random or otherwise generated. It may be used to display a sample of fonts, generate text for testing, or to spoof an e-mail spam filter.";
+let textContentArray = textContent.split(" ");
+
 let selectedMode = "";
 let selectedModeOption = "";
+let lastTypingTime = null;
 let timerInterval = null;
+
+let startTyping = false;
 // mode : mode-details
 const setting = {
     "Time": {
@@ -39,7 +45,6 @@ function generateMode(mode) {
 
 function generateModeDetails(mode, select = null) {
     const details = setting[mode];
-    console.log('details : ', mode);
     const optionName = `mode-${mode}`;
     
     // Retrieve previous selection from localStorage
@@ -106,6 +111,10 @@ function showTimerUI(time) {
 function startTimer() {
     if (timerInterval) return;
     let timeInc = parseInt(selectedModeOption);
+
+    // just for result purpose 
+    lastTypingTime = timeInc; 
+
     if (selectedMode == "Time" && timeInc > 0) {
         timerInterval = setInterval(() => {
             showTimerUI(timeInc);
@@ -120,16 +129,104 @@ function startTimer() {
 }
 
 function finishTyping() {
-    updateTypingBody("<h2 class='text-center'>Complete!</h2>");
+    const data = calculateWordsData();
+    const accuracy = ((data.correctwords / data.totalwords) * 100 );
+    const typingTime = lastTypingTime;
+    const speed = ((data.totalwords / typingTime) * 60);
+    const html = createResultTemplate(data, speed, accuracy, typingTime);
+    updateTypingBody(html);
 }
 
-const startBtn = document.getElementById("startBtn");
-startBtn.addEventListener("click", function() {
-    console.log('mode > ', selectedMode);
-    console.log('modeOption > ', selectedModeOption);
-    startTimer();
-})
+function createResultTemplate(data, speed, accuracy, typingTime) {
+    return `
+        <div class="d-flex justify-content-center align-items-center fs-2 p-5"> 
+            <table>
+                <tr>
+                    <td>Speed : </td>
+                    <td>${speed} WPH</td>
+                </tr>
+                <tr>
+                    <td>Accuracy : </td>
+                    <td>${accuracy.toFixed(2)}%</td>
+                </tr>
+                <tr>
+                    <td>Time : </td>
+                    <td>${typingTime} Sec</td>
+                </tr>
+                <tr>
+                    <td>Total Words : </td>
+                    <td>${data.totalwords}</td>
+                </tr>
+                <tr>
+                    <td>Correct Words : </td>
+                    <td>${data.correctwords}</td>
+                </tr>
+                <tr>
+                    <td>Incorrect Words : </td>
+                    <td>${data.incorrectwords}</td>
+                </tr>
+            </table>
+        </div>
+    `;
+}
 
+function calculateWordsData() {
+    const data = {
+        totalwords: 0,
+        correctwords: 0,
+        incorrectwords: 0,
+        totalletters: 0,
+        correctletters: 0,
+        incorrectletters: 0
+    };
+
+    const words = document.querySelectorAll('.word.typed');
+    words.forEach(word => {
+        const letters = word.children;
+        for (const letter of letters) {
+            if (letter.classList.contains('correct')) {
+                data.correctletters++;
+            } 
+            // else if (letter.classList.contains('incorrect')) {
+                // data.incorrectletters++;
+                // wrongword = true; }
+            else {
+                data.incorrectletters++;
+            }
+            data.totalletters++;
+        }
+        if (word.classList.contains('wrong')) {
+            data.incorrectwords++;
+        } else {
+            data.correctwords++;
+        }
+        data.totalwords++;
+    })
+
+    return data;
+}
+
+// ====== Load Typing Content ========
+function updateTypingContent(content) {
+    document.getElementById('typing-content').innerHTML = content;
+}
+
+function loadTextContent() {
+    let wordsHTML = "";
+    let active = true;
+    textContentArray.forEach((letters) => {
+        wordsHTML += `<div class="word ${active ? 'active' : ''}">`;
+        active = false;
+        for(let i = 0; i < letters.length; i++) {
+            const letter = letters[i];
+            wordsHTML += `<letter>${letter}</letter>`;
+        }
+        wordsHTML += `</div>`;
+    })
+    updateTypingContent(wordsHTML);
+}
+
+loadTextContent();
 
 // ======== Typing Feature ========
 function getActiveWord() {
@@ -140,14 +237,28 @@ function focusTypingCursor() {
     const word = getActiveWord();
     if (!word) return;
 
+    let letterIndex = 0;
     for (const letter of word.children) {
+        const lastLetterIndex = word.children.length - 1;
         if (!letter.classList.contains('correct') && !letter.classList.contains('incorrect')) {
             focusLetter(letter);
             return;
         } else {
             removeFocusLetter(letter);
         }
+        if (letterIndex === lastLetterIndex) {
+            focusEndLetter(letter);
+        }
+        letterIndex++;
     }
+}
+
+function focusEndLetter(element) {
+    element.classList.add('focus-end');
+}
+
+function removeFocusEndLetter(element) {
+    element.classList.remove('focus-end');
 }
 
 function focusLetter(element) {
@@ -175,7 +286,7 @@ function matchKeyPressToOriginal(value) {
     for (const letter of word.children) {
         if (!letter.classList.contains('correct') && !letter.classList.contains('incorrect')) {
             const expected = letter.textContent.trim();
-            letter.classList.remove('correct', 'incorrect', 'wrong');
+            letter.classList.remove('correct', 'incorrect');
             letter.classList.add(value === expected ? 'correct' : 'incorrect');
             break;
         }
@@ -188,30 +299,73 @@ function isWordComplete(word) {
     );
 }
 
+function markWrongWord(word) {
+    for(const letter of word.children) {
+        letter.classList.remove('focus');
+        if (letter.classList.length < 1 || letter.classList.contains('incorrect')) {
+            word.classList.add('wrong');
+            return;
+        }
+    }
+}
+
 function switchToNextWord() {
     const currentWord = getActiveWord();
     if (!currentWord) return;
 
+    markWrongWord(currentWord);
+    currentWord.classList.remove('active');
+    currentWord.classList.add('typed');
+    
+    for (const letter of currentWord.children) {
+        removeFocusEndLetter(letter);
+    }
+
     const nextWord = currentWord.nextElementSibling;
     if (!nextWord) return;
-
-    currentWord.classList.remove('active');
-    currentWord.classList.add('complete');
 
     nextWord.classList.add('active');
     clearFocusFromInactiveWords();
     focusTypingCursor();
 }
 
+function switchToBackLetter(word) {
+    if (!word) return;
+    let letters = word.children;
+    for (let i = letters.length - 1; i >= 0; i--) {
+        const letter = letters[i];
+        const previousLetter = letters[i - 1];
+        if (letter.classList.contains('focus-end')) {
+            letter.classList.remove('focus', 'focus-end', 'correct', 'incorrect');
+            letter.classList.add('focus');
+        } else if (letter.classList.contains('focus') && previousLetter) {
+            letter.classList.remove('focus', 'focus-end', 'correct', 'incorrect');
+            previousLetter.classList.remove('incorrect', 'correct', 'focus', 'focus-end');
+            previousLetter.classList.add('focus');
+            break;
+        }
+    }
+}
+
 function handleKeyPress(event) {
+    if (!startTyping) {
+        startTyping = true;
+        startTimer();
+    }
+    
     const key = event.key;
 
     if (key === ' ') {
         event.preventDefault(); // Prevent browser from scrolling
         const word = getActiveWord();
-        if (isWordComplete(word)) {
-            switchToNextWord();
-        }
+        switchToNextWord();
+        return;
+    }
+    // backspace 
+    if (key === 'Backspace') {
+        event.preventDefault(); // Prevent browser from scrolling
+        const word = getActiveWord();
+        switchToBackLetter(word);
         return;
     }
 
@@ -224,5 +378,5 @@ function handleKeyPress(event) {
 // ======== Initialize Typing Game ========
 document.addEventListener("DOMContentLoaded", () => {
     focusTypingCursor();
-    document.addEventListener("keypress", handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
 });
